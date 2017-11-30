3f4e3c54fdf866843bf4e0f1d23cdef2eb5d8c80
chema969 <chema969@hotmail.com>
2017-11-30 19:25:32 +0100

    ej3

--- ejercicio3.c
+++ ejercicio3.c
@@ -14,11 +14,14 @@ typedef struct params
     int t_esp;
     int t_rest;
 } parametros;
-int FIFOvive(parametros *FIFO));
+int FIFOvive(parametros *FIFO);
 void entraFINAL(parametros *FIFO,parametros p,int tam);
+void ponevalores(parametros *FIFO,parametros *procesos,int cicloreloj,int * tam);
+void reordenar(parametros *FIFO,int tam);
+void imprime(parametros *FIFO, int tam);
 void main(){
     
-    int i,cicloreloj=0,q,tam;
+    int i,cicloreloj=0,q,tam,j;
     // Inicialización de los datos de procesos
     parametros procesos[NPROC];    
     parametros *FIFO;
@@ -51,30 +54,66 @@ void main(){
     printf("\nProceso | t_comienzo | t_final | t_retorno | t_espera\n");
     printf("-------------------------------------------------------\n");
     FIFO=(parametros*)malloc(sizeof(parametros));
+    FIFO[0]=procesos[0];
     tam=0;
-    while(FIFOvive(FIFO)){
-      q=3;
-      while((q)&&(FIFO[0].t_rest)){
-        q--;
+    while(tam!=-1){  
+      q=0;
+      if(FIFO[0].t_rest==FIFO[0].t_ejec){  
+       for (j=0;j<NPROC;j++){ 
+        if(strcmp(FIFO[0].nombre,procesos[j].nombre)==0) procesos[j].t_com=cicloreloj; 
+         }
+       }
+      while((q!=QUANTUM)&&(FIFO[0].t_rest)){
+        q++;
         FIFO[0].t_rest--; 
         cicloreloj++;
         for (i=1;i<NPROC;i++){ 
-          if(cicloreloj==procesos[i].t_lleg)    {tam++;FIFO=entraFINAL(FIFO,procesos[i],tam);}
+          if(cicloreloj==procesos[i].t_lleg)    {tam++;entraFINAL(FIFO,procesos[i],tam);}
         }
-      
-
-
+       }
+      if(FIFO[0].t_rest==0) ponevalores(FIFO,procesos,cicloreloj,&tam);
+      else reordenar(FIFO,tam);
     }
+
+   for(i=0; i< NPROC; i++)
+    {
+        procesos[i].t_ret = procesos[i].t_fin - procesos[i].t_lleg;
+        procesos[i].t_esp = procesos[i].t_ret - procesos[i].t_ejec;
+
+        printf("   %s \t    %d \t\t %d \t   %d \t      %d\n", procesos[i].nombre,procesos[i].t_com, procesos[i].t_fin, procesos[i].t_ret, procesos[i].t_esp);
+    }    
 }
 
-int FIFOvive(parametros *FIFO)){
+int FIFOvive(parametros *FIFO){
     if(FIFO==NULL) return 0;
     else return 1;
     }
 
 void entraFINAL(parametros *FIFO,parametros p,int tam){
-     parametros *PIPO;
-     PIPO = (parametros*) realloc (FIFO, sizeof(parametros))
-     PIPO[tam]=p;
-     return PIPO;
+     FIFO = (parametros*) realloc (FIFO, sizeof(parametros)*(tam+1));
+     FIFO[tam]=p;
 }
+
+void ponevalores(parametros *FIFO,parametros *procesos,int cicloreloj,int * tam){
+    int j;
+     for (j=0;j<NPROC;j++){ 
+        if(strcmp(FIFO[0].nombre,procesos[j].nombre)==0) procesos[j].t_fin=cicloreloj;
+       }
+    int i;
+      for(i=0;i<(*tam);i++) FIFO[i]=FIFO[i+1];
+      FIFO=(parametros*) realloc (FIFO, sizeof(parametros)*(*tam));
+       *tam=*tam-1;
+} 
+
+void reordenar(parametros *FIFO,int tam){
+     int i;
+     parametros aux=FIFO[0];     
+     for(i=0;i<tam;i++) FIFO[i]=FIFO[i+1];
+     FIFO[i]=aux;
+}
+
+void imprime(parametros *FIFO, int tam){
+     int i;
+     for(i=tam;i>-1;i--) printf(" %s ",FIFO[i].nombre);
+     printf("%d\n",tam);
+    }
